6e124d66d139a447441ebb966156a3362d62cb79
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2021-02-26 11:18:35 +0100

    slut på lektionen 2021-02-26

---
+++ src/Komplex.java
@@ -0,0 +1,11 @@
+
+public class Komplex {
+    public double real;
+    public double imagi;
+
+    public Komplex(double real, double imagi) {
+        this.real = real;
+        this.imagi = imagi;
+    }
+
+}
